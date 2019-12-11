92585a9f44238dd7c5e65ec404319d4d8378656b
Ian Staal <staalij@gmail.com>
2019-12-11 10:47:50 -0700

    Both input methods finished. All javascript done.

--- script.js
+++ script.js
@@ -4,27 +4,25 @@ let minsput = form.elements.minutes;
 let daysput = form.elements.days;
 let hrsput = form.elements.hours;
 let secput = form.elements.seconds;
-let endmonth = form.elements.monthcountto;
-let endday = form.elements.daycountto;
 let endhour = form.elements.hourcountto;
 let endminute = form.elements.minutecountto;
 let endsecond = form.elements.secondcountto;
-let endyear = form.elements.yearcountto;
 let clock = document.getElementById("clock");
 let local = document.getElementById("local");
 let localmins = 0;
 let localhrs = 0;
 let localsec = 0;
 let currentdate = new Date();
-let checkbox = form.elements.toggledate;
-let checked;
-
+let checked = false;
+let hrs = 0;
+let mins = 0;
+let sec = 0;
+// end of very large list of variables I probably didn't even use all of
 
 function ready() {
     console.log("ready");
     
-    
-
+// Gets local time
         setInterval(() => {
             currentdate = new Date();
             localmins = currentdate.getMinutes();
@@ -33,54 +31,62 @@ function ready() {
             local.innerHTML = localhrs + ":" + localmins + ":" + localsec;
         }, 100);
 
+// Start of main segment
     submit.onclick = function run() {
-
         checked = document.getElementById('box').checked;
 
-        if (checked == true) {
-            mins = minsput.value;
-        hrs = hrsput.value;
-        sec = secput.value;
-        days = daysput.value;
-        if (minsput == undefined || mins == 0) { mins = '00'; };
-        if (hrsput == undefined || hrs == 0) { hrs = "00"; };
-        if (secput == undefined || sec == 0) { sec = '00'; };
-        if (daysput == undefined || days == 0) { days = '00'; };
         console.log(checked);
         
+// Block of code that checks if the box is checked and sets variables accordingly
+        if (checked) {
+            if (endminute == undefined || mins == 0) { mins = '00'; };
+            if (endhour == undefined || hrs == 0) { hrs = "00"; };
+            if (endsecond == undefined || sec == 0) { sec = '00'; };
+            mins = endminute.value - localmins;
+            sec = endsecond.value - localsec;
+            hrs = endhour.value - localhrs;
+            days = 0;
 
         } else {
-
-
-        mins = minsput.value;
-        hrs = hrsput.value;
-        sec = secput.value;
-        days = daysput.value;
-        if (minsput == undefined || mins == 0) { mins = '00'; };
-        if (hrsput == undefined || hrs == 0) { hrs = "00"; };
-        if (secput == undefined || sec == 0) { sec = '00'; };
-        if (daysput == undefined || days == 0) { days = '00'; };
-
-
+// Block of code that sets variables if there is not a set end time
+            mins = minsput.value;
+            hrs = hrsput.value;
+            sec = secput.value;
+            days = daysput.value;
+            if (minsput == undefined || mins == 0) { mins = '00'; };
+            if (hrsput == undefined || hrs == 0) { hrs = "00"; };
+            if (secput == undefined || sec == 0) { sec = '00'; };
+            if (daysput == undefined || days == 0) { days = '00'; };
         }
-
-        
-
+    
+// Converts time
         if (mins >= 60) { mins -= 60; hrs += 1; };
         if (hrs >= 24) { hrs -= 24; days += 1; };
         if (sec >= 60) { sec -= 60; mins += 1; };
-            
+           
+        
+// Counts down timer
         let i = setInterval(() => {
             sec = sec- 1;
-            if (sec == -1) { sec = 59; mins = mins - 1; };
-            if (mins == -1) { mins = 59; hrs = hrs - 1; };
-            if (hrs == -1) { hrs = 23; days = days - 1; };
-            if (days == -1) { window.location.href = 'party.html' ;days = 0;};
-        
-            clock.innerHTML = days + ":" + hrs + ":" + mins + ":" + sec;
+            if (sec <= -1) { sec = 59; mins = mins - 1; };
+            if (mins <= -1) { mins = 59; hrs = hrs - 1; };
+            if (hrs <= -1) { hrs = 23; days = days - 1; };
+            if (days <= -1) { window.location.href = 'party.html' ;days = 0;};
+// Converts values to strings and adds 0 on the beginning if a number below 10 is displayed
+            sec = `${sec}`;
+            mins = `${mins}`;
+            hrs = `${hrs}`;
+            days = `${days}`
+            if (sec.length == 1) {sec = '0' + sec;}
+            if (mins.length == 1) {mins = '0' + mins;}              
+            if (hrs.length == 1) {hrs = '0' + hrs;}               
+            if (days.length == 1) {days = '0' + days;}
+// Displays countdown
+            clock.innerHTML = `${days}` + ":" + `${hrs}` + ":" + `${mins}` + ":" + `${sec}`;
 
-        }, 1);
+        }, 1000);
     
+// resets interval when start is clicked again
         submit.onclick = function() {
             clearInterval(i);
             run();
